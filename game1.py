15612a464051976228a347963d4e18e89d70b77c
tzixi <tanzixi9@gmail.com>
2022-05-08 18:14:49 +0800

    Added elements and sky

--- game1.py
+++ game1.py
@@ -9,16 +9,15 @@ pygame.display.set_caption('Runner')
 #controls framerate
 clock = pygame.time.Clock()
 
+sky_surface = pygame.image.load('graphics/Sky.png') # this is the regular surface, the tuple inside is width, height
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit() #opposite to pygame init
             exit() #exits the loop and removes 'video system not initialised'
-    #draw all of our elements
-    #update everything
+    
+    screen.blit(sky_surface,(0,0)) #surface, position
+    
     pygame.display.update() #updates the display
     clock.tick(60) #fps
-
-
-
-
